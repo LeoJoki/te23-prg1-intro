507b12b6aff9eafe189977ac5e1c805be4404406
LJoki06 <leo.jokijarvi@elev.ga.ntig.se>
2024-09-12 09:41:04 +0200

    art

--- kung.py
+++ kung.py
@@ -5,13 +5,79 @@ play_game = True
 p1_wins = 0
 p2_wins = 0
 
+dice_one = """            _____________
+            |           |
+            |           |
+            |     *     |
+            |           |
+            |___________|"""
+
+dice_two = """            _____________
+            |           |
+            |     *     |
+            |           |
+            |     *     |
+            |___________|"""
+
+dice_three = """            _____________
+            |           |
+            |   *       |
+            |     *     |
+            |       *   |
+            |___________|"""
+
+dice_four = """            _____________
+            |           |
+            |   *   *   |
+            |           |
+            |   *   *   |
+            |___________|"""
+
+dice_five = """            _____________
+            |           |
+            |   *   *   |
+            |     *     |
+            |   *   *   |
+            |___________|"""
+
+dice_six = """            _____________
+            |           |
+            |   *   *   |
+            |   *   *   |
+            |   *   *   |
+            |___________|"""
+
 while play_game:
     input("spelare ETT tryck på enter för att rulla tärningen")
     p1_roll = randint(1, 6)
+    if p1_roll == 1:
+        print(f"{dice_one}")
+    if p1_roll == 2:
+        print(f"{dice_two}")
+    if p1_roll == 3:
+        print(f"{dice_three}")
+    if p1_roll == 4:
+        print(f"{dice_four}")
+    if p1_roll == 5:
+        print(f"{dice_five}")
+    if p1_roll == 6:
+        print(f"{dice_six}")
     print(f"spelare ETT fick en {p1_roll}a")
 
     input("spelare TVÅ tryck på enter för att rulla tärningen")
     p2_roll = randint(1, 6)
+    if p2_roll == 1:
+        print(f"{dice_one}")
+    if p2_roll == 2:
+        print(f"{dice_two}")
+    if p2_roll == 3:
+        print(f"{dice_three}")
+    if p2_roll == 4:
+        print(f"{dice_four}")
+    if p2_roll == 5:
+        print(f"{dice_five}")
+    if p2_roll == 6:
+        print(f"{dice_six}")
     print(f"spelare TVÅ fick en {p2_roll}a")
 
     if p1_roll > p2_roll:
@@ -29,3 +95,47 @@ while play_game:
     elif p2_wins == 3:
         play_game = False
         print("spelare TVÅ har vunnit spelet!")
+
+
+
+#_____________
+#|           |
+#|           |
+#|     *     |
+#|           |
+#|___________|
+
+#_____________
+#|           |
+#|     *     |
+#|           |
+#|     *     |
+#|___________|
+
+#_____________
+#|           |
+#|   *       |
+#|     *     |
+#|       *   |
+#|___________|
+
+#_____________
+#|           |
+#|   *   *   |
+#|           |
+#|   *   *   |
+#|___________|
+
+#_____________
+#|           |
+#|   *   *   |
+#|     *     |
+#|   *   *   |
+#|___________|
+
+#_____________
+#|           |
+#|   *   *   |
+#|   *   *   |
+#|   *   *   |
+#|___________|
