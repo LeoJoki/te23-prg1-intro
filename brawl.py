d285b1575e855c99f856cebafb7d96a84be32a10
LJoki06 <leo.jokijarvi@elev.ga.ntig.se>
2024-10-03 09:33:26 +0200

    Gjorde spel coolare

--- brawl.py
+++ brawl.py
@@ -1,11 +1,25 @@
 from random import randint
+from colorama import Fore, Back, Style
 
-p1_name = input("Spelare 1, vad är ditt namn?: ")
+input(Style.DIM + Fore.RED + """
+____________  ___  _    _ _     
+| ___ \ ___ \/ _ \| |  | | |    
+| |_/ / |_/ / /_\ \ |  | | |    
+| ___ \    /|  _  | |/\| | |    
+| |_/ / |\ \| | | \  /\  / |____
+\____/\_| \_\_| |_/\/  \/\_____/
+      
+Tryck enter för att starta""")
+
+Fore.RESET
+
+p1_name = input(Fore.RESET + "Spelare 1, vad är ditt namn?: ")
 
 p1_class_choice = input(f"""{p1_name}, välj en klass:
-      1: Krigare
-      2: Helare
-      3: Tjuv
+      1:{Fore.RED} Krigare, Förmåga: Gör 1d4 extra skada{Fore.RESET}
+      2:{Fore.BLUE} Helare, Förmåga: Gör att båda spelare ignorerar rundans skada{Fore.RESET}
+      3:{Fore.MAGENTA} Tjuv, Förmåga: Byt plats på tärningskasten{Fore.RESET}
+      4:{Fore.YELLOW} Gambler, Förmåga: 1/10 chans att antingen direkt vinna eller förlora{Fore.RESET}
       """)
 if p1_class_choice == "1":
     p1_class = "Krigare"
@@ -13,15 +27,18 @@ elif p1_class_choice == "2":
     p1_class = "Helare"
 elif p1_class_choice == "3":
     p1_class = "Tjuv"
+elif p1_class_choice == "4":
+    p1_class = "Gambler"
 else:
     print("Du skrev inte ett giltigt nummer, välj en klass.")
 
 p2_name = input("Spelare 2, vad är ditt namn?: ")
 
 p2_class_choice = input(f"""{p2_name}, välj en klass:
-      1: Krigare
-      2: Helare
-      3: Tjuv
+      1:{Fore.RED} Krigare, Förmåga: Gör 1d4 extra skada{Fore.RESET}
+      2:{Fore.BLUE} Helare, Förmåga: Gör att båda spelare ignorerar rundans skada{Fore.RESET}
+      3:{Fore.MAGENTA} Tjuv, Förmåga: Byt plats på tärningskasten{Fore.RESET}
+      4:{Fore.YELLOW} Gambler, Förmåga: 1/10 chans att antingen direkt vinna eller förlora{Fore.RESET}
       """)
 if p2_class_choice == "1":
     p2_class = "Krigare"
@@ -29,6 +46,8 @@ elif p2_class_choice == "2":
     p2_class = "Helare"
 elif p2_class_choice == "3":
     p2_class = "Tjuv"
+elif p2_class_choice == "4":
+    p2_class = "Gambler"
 else:
     print("Du skrev inte ett giltigt nummer, välj en klass.")
 
@@ -86,9 +105,9 @@ game_round = 0
 
 while game:
     game_round += 1
-    print(f"Runda {game_round} börjar nu!")
-    print(f"""{p1_class} {p1_name}s liv: {p1_life}
-{p2_class} {p2_name}s liv: {p2_life}""")
+    print(f"{Fore.BLUE}Runda {game_round} börjar nu!{Fore.RESET}")
+    print(f"""{Fore.GREEN}{p1_class} {p1_name}s liv: {p1_life}
+{p2_class} {p2_name}s liv: {p2_life}{Fore.RESET}""")
 
     input(f"{p1_class} {p1_name}, tryck på enter för att rulla tärningen")
     p1_roll = randint(1, 6)
@@ -104,7 +123,7 @@ while game:
         print(f"{dice_five}")
     if p1_roll == 6:
         print(f"{dice_six}")
-    print(f"{p1_class} {p1_name} fick en {p1_roll}a")
+    print(f"{Fore.BLUE}{p1_class} {p1_name} fick en {p1_roll}a{Fore.RESET}")
 
     input(f"{p2_class} {p2_name}, tryck på enter för att rulla tärningen")
     p2_roll = randint(1, 6)
@@ -120,11 +139,11 @@ while game:
         print(f"{dice_five}")
     if p2_roll == 6:
         print(f"{dice_six}")
-    print(f"{p2_class} {p2_name} fick en {p2_roll}a")
+    print(f"{Fore.BLUE}{p2_class} {p2_name} fick en {p2_roll}a{Fore.RESET}")
 
     if p1_ability == True:
-        p1_use_ability = input(f"{p1_class} {p1_name} vill du använda din klass förmåga? Y/N: ")
-        if p1_use_ability == "Y" or "y" or "yes" or "Yes" or "Ja" or "ja":
+        p1_use_ability = input(f"{Fore.BLUE}{p1_class} {p1_name} vill du använda din klass förmåga? Y/N: {Fore.RESET}")
+        if p1_use_ability == "y":
             if p1_class == "Tjuv":
                 temp = p1_roll
                 p1_roll = p2_roll
@@ -140,12 +159,20 @@ while game:
                 p2_roll = 0
                 p1_ability = False
                 print(f"{p1_class} {p1_name} helade sina skador!")
-        elif p1_use_ability == "N":
-            print(f"{p1_class} {p1_name} valde att inte använda sin klass förmåga")
+            elif p1_class == "Gambler":
+                gamble = randint(1,10)
+                print(f"{p1_class} {p1_name} försökte spela med sit liv och fick en {gamble}a")
+                if gamble == 10:
+                    p2_life = 0
+                elif gamble == 1:
+                    p1_life = 0
+                p1_ability = False
+        elif p1_use_ability == "n":
+            print(f"{Fore.BLUE}{p1_class} {p1_name} valde att inte använda sin klass förmåga{Fore.RESET}")
     
     if p2_ability == True:
-        p2_use_ability = input(f"{p2_class} {p2_name} vill du använda din klass förmåga? Y/N: ")
-        if p2_use_ability == "Y" or "y" or "yes" or "Yes" or "Ja" or "ja":
+        p2_use_ability = input(f"{Fore.BLUE}{p2_class} {p2_name} vill du använda din klass förmåga? Y/N: {Fore.RESET}")
+        if p2_use_ability == "y":
             if p2_class == "Tjuv":
                 temp = p2_roll
                 p2_roll = p1_roll
@@ -161,8 +188,16 @@ while game:
                 p2_roll = 0
                 p2_ability = False
                 print(f"{p2_class} {p2_name} helade sina skador!")
-        elif p2_use_ability == "N":
-            print(f"{p2_class} {p2_name} valde att inte använda sin klass förmåga")
+            elif p2_class == "Gambler":
+                gamble = randint(1,10)
+                print(f"{p2_class} {p2_name} försökte spela med sit liv och fick en {gamble}a")
+                if gamble == 10:
+                    p1_life = 0
+                elif gamble == 1:
+                    p2_life = 0
+                p2_ability = False
+        elif p2_use_ability == "n":
+            print(f"{Fore.BLUE}{p2_class} {p2_name} valde att inte använda sin klass förmåga{Fore.RESET}")
 
 
     if p1_roll > p2_roll:
